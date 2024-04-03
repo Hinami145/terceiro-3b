6f97b39745a011abe24de7db6cb4357969bc60b2
Hinamii/ Amanda <105732005+Hinami145@users.noreply.github.com>
2024-04-03 09:01:15 -0300

    Update main.js
    new hair new tee

--- amanda 3b/main.js
+++ amanda 3b/main.js
@@ -30,6 +30,7 @@ function calculaTempo(tempoObjetivo1){
     let tempoAtual = newDate();
     let tempoFinal = tempoObjetivo1-tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor( segundos/ 60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
 
@@ -37,4 +38,11 @@ function calculaTempo(tempoObjetivo1){
     minutos %= 60;
     horas %= 24;
 
+    if (tempoFinal > 0){
+        return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    }
+    else{
+        return "Prazo Finalizado";
+    }
 }
+
